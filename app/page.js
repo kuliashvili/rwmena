5e3f79990a3bde5f6165c119b98df7330f37eb57
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-08-27 13:51:08 +0400

    wewegw

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import blogData from "./data/blogs.json";
 import Button from "./components/button/button";
 import quotes from "./data/quotes";
 import Link from "next/link";
+import Blog from "./components/blog/blog.js";
 
 export default function Home() {
   const [currentQuoteIndex, setQurrentQuoteIndex] = useState(0);
@@ -45,20 +46,67 @@ export default function Home() {
           {quotes[currentQuoteIndex]}
         </h1>
       </div>
+      <div className="blogs container">
+        {blogData.map((blog) => (
+          <Blog key={blog.id} title={blog.title} image={blog.image} />
+        ))}
+      </div>
       <div className="container">
-        <h1 className="heading" >ბლოგები</h1>
+        <h1 className="heading">ბლოგები</h1>
         <div className="blogs" style={{ paddingTop: "40px" }}>
           {posts.map((post) => (
             <Link className="blog" href={`/posts/${post.id}`} key={post.id}>
               <div key={post.id}>
-                <h2 className="blog-header">{post.title}</h2> 
+                <h2 className="blog-header">{post.title}</h2>
                 {/* <p>{post.body}</p> */}
               </div>
             </Link>
           ))}
         </div>
       </div>
-      <Button className='buttonn' redirectTo={"/info"} text={"Helloworld"} />
+      <Button className="buttonn" redirectTo={"/info"} text={"Helloworld"} />
     </>
   );
 }
+
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import Image from "next/image";
+// import "./styles/globals.css";
+// import "./styles.css";
+// import Blog from "./components/blog/blog.js";
+// import blogData from "./data/blogs.json";
+// import Button from "./components/button/button";
+// import quotes from "./data/quotes";
+
+// export default function Home() {
+//   const [currentQuoteIndex, setQurrentQuoteIndex] = useState(0);
+//   const [fadeClass, setFadeClass] = useState("fade-in");
+
+//   useEffect(() => {
+//     const quoteInterval = setInterval(() => {
+//       setFadeClass("");
+//       setTimeout(() => {
+//         setQurrentQuoteIndex((prevIndex) => (prevIndex + 1) % quotes.length);
+//         setFadeClass("fade-in")
+//       }, 1000);
+
+//     }, 10000);
+
+//     return () => clearInterval(quoteInterval);
+//   }, []);
+//   return (
+//     <>
+//       <div className="main">
+//         <h1 className={`main-header ${fadeClass}`}>{quotes[currentQuoteIndex]}</h1>
+//       </div>
+//       <div className="blogs container">
+//         {blogData.map((blog) => (
+//           <Blog key={blog.id} title={blog.title} image={blog.image} />
+//         ))}
+//       </div>
+//       <Button redirectTo={"/info"} text={"Helloworld"} />
+//       <div className="konteinrtest container">izi</div>
+//     </>
+//   );
+// }
